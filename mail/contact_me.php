0f377e8e17c34305278a288df3ebec902e9d96b3
Divya354 <dsk5df@virginia.edu>
2016-10-04 14:07:19 -0400

    mess with contact form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -10,17 +10,34 @@ if(empty($_POST['name'])  		||
 	return false;
    }
 
+
+
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-// Create the email and send the message
-$to = 'divya354@yahoo.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-return true;
+file_put_contents("messages.txt", $name, FILE_APPEND);
+file_put_contents("messages.txt", "\n", FILE_APPEND);
+
+file_put_contents("messages.txt", $email_address, FILE_APPEND);
+file_put_contents("messages.txt", "\n", FILE_APPEND);
+
+file_put_contents("messages.txt", $phone, FILE_APPEND);
+file_put_contents("messages.txt", "\n", FILE_APPEND);
+
+file_put_contents("messages.txt", $message, FILE_APPEND);
+file_put_contents("messages.txt", "\n", FILE_APPEND);
+file_put_contents("messages.txt", "\n", FILE_APPEND);
+
+//
+// // Create the email and send the message
+// $to = 'divya354@yahoo.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+// $email_subject = "Website Contact Form:  $name";
+// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+// $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+// $headers .= "Reply-To: $email_address";
+// mail($to,$email_subject,$email_body,$headers);
+ return true;
 ?>
